ddff42b4d2366195334fec81ffdcbcf916d2acb3
sanghwiback <sanghwiback@gmail.com>
2024-02-12 11:24:28 +0900

    SideBar css enhancements

--- sign-up/src/components/Header.jsx
+++ sign-up/src/components/Header.jsx
@@ -1,8 +1,15 @@
-export default function Header({onClick}) {
+export default function Header({isOpen, toggleSideBarOpen}) {
+  const headerClass = "flex h-200px m-7";
+  const headerButtonClass = "rounded-full bg-blue-50 p-2 mr-2 ml-2 flex-none align-middle";
   return (
-    <div className="flex h-200px m-7">
-      <button className="rounded-full bg-blue-50 p-2 mr-2 ml-2" onClick={onClick}>{'<'}</button>
-      <p>Header</p>
+    <div className={headerClass}>
+      <button 
+        className={headerButtonClass} 
+        onClick={() => toggleSideBarOpen(!isOpen)}
+      >
+        {isOpen ? "<" : ">"}
+      </button>
+      <div className="my-auto">Header</div>
     </div>
   );
 }
--- sign-up/src/components/SideBar.jsx
+++ sign-up/src/components/SideBar.jsx
@@ -9,18 +9,13 @@ function SidebarList() {
 }
 
 export default function Sidebar({ isOpen, toggleOpen }) {
+  const sideBarClasses = `${isOpen && 'p-4'} left-0 bg-white h-full overflow-auto ease-in-out duration-300 ${
+    !isOpen && "-left-64"
+  }`;
+
   return (
-    <div
-      className={`transform top-0 left-0 w-64 bg-white fixed h-full overflow-auto ease-in-out transition-all duration-300 z-30 ${
-        isOpen ? "translate-x-0" : "-translate-x-full"
-      }`}
-    >
-      <div className="p-4">
-        <button className="text-black" onClick={toggleOpen}>
-          {isOpen ? "Close Sidebar" : "Open Sidebar"}
-        </button>
-        {isOpen && <SidebarList />}
-      </div>
+    <div className={sideBarClasses}>
+      {isOpen && <SidebarList />}
     </div>
   );
 }
